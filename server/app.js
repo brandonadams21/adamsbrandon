64c546c89a31773ac9f55ab4747ab29a4eb508d8
Brandon Adams <brandonadams@Brandons-MBP.attlocal.net>
2023-08-29 21:22:00 -0500

    inital commit

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.post("/contact-form", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
